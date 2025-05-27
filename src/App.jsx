3f0b5d3a52f58227819ed465a94200380217091a
Hanna Ponomarenko <pnmrnk.anna@gmail.com>
2025-05-27 13:53:03 +0200

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,29 @@ import Statistics from "./components/Statistics/Statistics";
 import article from "./data/article.json";
 import stats from "./data/stats.json";
 import friends from "./data/friends.json";
+import Section from "./components/Section/Section";
+import Container from "./components/Container/Container";
+
 function App() {
   return (
     <>
-      <h1>bc-75</h1>
-      <BlogCard
-        avatar={article.avatar}
-        postedAt={article.postedAt}
-        poster={article.poster}
-        tag={article.tag}
-        title={article.title}
-        description={article.description}
-        name={article.name}
-      />
-      {/* <BlogCard {...article} /> */}
-      <Statistics stats={stats} title="Main Statistics" />
-      <Friendlist friends={friends}/>
+      <Section>
+        <Container>
+          <h1>bc-75</h1>
+          <BlogCard
+            avatar={article.avatar}
+            postedAt={article.postedAt}
+            poster={article.poster}
+            tag={article.tag}
+            title={article.title}
+            description={article.description}
+            name={article.name}
+          />
+          {/* <BlogCard {...article} /> */}
+          <Statistics stats={stats} title="Main Statistics" />
+          <Friendlist friends={friends} />
+        </Container>
+      </Section>
     </>
   );
 }
--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,6 @@
+import { formateDateToNow } from "../../helpers/formate-date";
+import style from "./BlogCard.module.css";
+
 const BlogCard = ({
   avatar,
   postedAt,
@@ -8,10 +11,24 @@ const BlogCard = ({
   name,
 }) => {
   return (
-    <div>
-      BlogCard
-      <img src={poster} alt={tag} />
-      <p>{description}</p>
+    <div className={style.card}>
+      <div className={style.cardHeader}>
+        <img className={style.cardPoster} src={poster} alt={tag} />
+      </div>
+      <div className={style.cardBody}>
+        <span className={style.tag}>{tag}</span>
+        <h2 className={style.cardTitle}>{title}</h2>
+        <p className={style.cardText}>{description}</p>
+      </div>
+      <div className={style.cardFooter}>
+        <div className={style.userBox}>
+          <img className={style.avatar} src={avatar} alt={name} />
+        </div>
+        <div>
+          <h3 className={style.userName}>{name}</h3>
+          <span className={style.date}>{formateDateToNow(postedAt)}</span>
+        </div>
+      </div>
     </div>
   );
 };
