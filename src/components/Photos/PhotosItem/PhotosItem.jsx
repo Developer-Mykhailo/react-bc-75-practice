99129341beeaac1ed5e1bbf78163389b8cfb5043
Danya76131 <danylo.galetskiy@gmail.com>
2025-06-06 14:29:17 +0200

    image

--- src/components/Photos/PhotosItem/PhotosItem.jsx
+++ src/components/Photos/PhotosItem/PhotosItem.jsx
@@ -1,12 +1,16 @@
 import s from "./PhotosItem.module.css";
 
-const PhotosItem = ({ color, imgSize, alt }) => {
+const PhotosItem = ({ color, imgSize, alt, openModal, large }) => {
   return (
     <div
       className={s.thumb}
       style={{ backgroundColor: color, borderColor: color }}
     >
-      <img src={imgSize} alt={alt} />
+      <img
+        src={imgSize}
+        alt={alt}
+        onClick={() => openModal({ src: large, alt })}
+      />
     </div>
   );
 };
--- src/components/Photos/PhotosList/PhotosList.jsx
+++ src/components/Photos/PhotosList/PhotosList.jsx
@@ -2,13 +2,19 @@ import Grid from "../../Grid/Grid";
 import GridItem from "../../GridItem/GridItem";
 import PhotosItem from "../PhotosItem/PhotosItem";
 
-const PhotosList = ({ imagesList }) => {
+const PhotosList = ({ imagesList, openModal }) => {
   return (
     <Grid>
-      {imagesList.map(({ src: { medium }, id, alt, avg_color }) => {
+      {imagesList.map(({ src: { medium, large }, id, alt, avg_color }) => {
         return (
           <GridItem key={id}>
-            <PhotosItem color={avg_color} imgSize={medium} alt={alt} />
+            <PhotosItem
+              color={avg_color}
+              imgSize={medium}
+              large={large}
+              alt={alt}
+              openModal={openModal}
+            />
           </GridItem>
         );
       })}
